6ce4f02a93c8f692189195a2c977f7d27e6a8721
waakwaan <56305864+waakwaan@users.noreply.github.com>
2019-12-16 15:20:52 +0100

    Revert "Update functions.py"
    This reverts commit 315812243919cdb70aa8a619b5a37b31d26f67e4.

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-#yeetus that feetus and deletus my penus
 otto = list(range(1,13))
 eng = list(range(41,53))
 spa = list(range(21, 33))
@@ -161,7 +160,7 @@ def assignEmpire(player_count):
 
 def empireToPlayer(empire):
     if empire  == flagDict.keys:
-        pass
+        
     else:
         return "Doesn't belong to any player"
         
